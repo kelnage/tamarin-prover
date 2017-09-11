66abee406aba4981f126e2cba1807ef3a1d9da7c
Nick Moore <nicholas.moore@cs.ox.ac.uk>
2017-09-11 09:18:36 +0100

    Fix warnings and correct comment.
    Removed unused imports and made an unused variable _.

--- lib/theory/src/Theory/Tools/GraphExecution.hs
+++ lib/theory/src/Theory/Tools/GraphExecution.hs
@@ -11,22 +11,16 @@ module Theory.Tools.GraphExecution (
     graphTheoryExec
   ) where
 
-import qualified Data.Set        as S
 import           Data.List       (nub)
 import qualified Data.Map        as M
-import           Data.Monoid     (Any(..))
 
 import qualified Text.Dot        as D
 
-import           Control.Basics
 import           Control.Monad.Reader
-import           Control.Monad.State      (StateT, evalStateT)
 
 import qualified Extension.Data.Label as L
 
 import           Theory
-import           Theory.Model
-import           Term.Unification
 
 -- import           Debug.Trace
 
@@ -35,7 +29,7 @@ graphTheoryExec :: ClosedTheory -> String
 graphTheoryExec thy = D.showDot (createExecutionGraph thy)
 
 -- | Given a theory, take its rules and for every premise, check which rules have conclusions that can unify
--- with that premise. For each of those rules, and an edge to the graph.
+-- with that premise. For each of those rules, add an edge to the graph.
 -- TODO: allow rules to be automatically excluded from the graph based on a rule name pattern? (Such as reveal)
 createExecutionGraph :: ClosedTheory -> D.Dot ()
 createExecutionGraph thy = do
@@ -56,7 +50,7 @@ createExecutionGraph thy = do
     sequence_ [ D.edge (fm M.! src) (fm M.! dst) sty | (src,dst,sty) <- edges ]
     return ()
   where
-    edgeStyle (Fact tag ts) = case (factTagName tag, factTagMultiplicity tag) of
+    edgeStyle (Fact tag _) = case (factTagName tag, factTagMultiplicity tag) of
         ("In", _)          -> [("color", "red"), ("style", "dotted")]
         (_,    Persistent) -> [("color", "blue"), ("style", "dashed")]
         _                  -> [("color", "black")]
